a24dedf7e5822885c4d3849819dc003de101cef7
Stephen Thomas <sthomas1618@gmail.com>
2011-09-07 18:28:55 -0700

    initial site setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Majorleaguebetting::Application.config.secret_token = '873c8d9902621739ce2a14c1d53507306a4d8d9bcb0298eb4e220e5d8ed936529fb30025b022e99c086386eb6a04788a06692cf225cfa356d670ae5f0566e3a5'
