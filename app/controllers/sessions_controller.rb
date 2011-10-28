dd47566a865117104da7ee1301a8299d2c3de07b
Stephen Thomas <sthomas1618@gmail.com>
2011-10-28 13:57:53 -0700

    Log in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
 class SessionsController < ApplicationController
   
   def create
-    
+  user = User.authenticate(params[:session][:username],
+                           params[:session][:password])
+  if user.nil?
+    # Create an error message and re-render the signin form.
+  else
+    # Sign the user in and redirect to the user's show page.
   end
+end
   
 end
