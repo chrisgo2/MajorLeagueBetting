8c84b1cc15e5ac5cd5b070bf9dbc3c8438233c2c
ckian <ckian@mail.csuchico.edu>
2011-11-04 13:25:27 -0700

    getting rid of changes before pull

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Majorleaguebetting::Application.routes.draw do
 
   get "users/new"
   
+  
+  
+  root :to => "home#public"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +56,7 @@ Majorleaguebetting::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#public"
+  
 
   # See how all your routes lay out with "rake routes"
 
