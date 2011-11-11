2d56915a0cc58ba8f5b62a27892890b2df422263
Sean Allen <gallonallen@gmail.com>
2011-11-10 22:24:48 -0800

    Added Database Shit

---
+++ db/migrate/20111110233337_create_game_bets_h2hs.rb
@@ -0,0 +1,14 @@
+class CreateGameBetsH2hs < ActiveRecord::Migration
+  def change
+    create_table :game_bets_h2hs do |t|
+      t.integer :user_id
+      t.integer :game_id
+      t.integer :team_id
+      t.integer :wager
+      t.boolean :status
+      t.boolean :won_or_lost
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/game_bets_h2h_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe GameBetsH2h do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
