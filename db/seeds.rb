021fc71a91c9cbff48e27696717ff5f125ea662d
Sean Allen <gallonallen@gmail.com>
2011-11-16 12:29:50 -0800

    Seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+League.create :name => "National Football League",
+               :short_name => "NFL",
+               :sport => "Football",
+               :logo_path => "app/assets/images/nfl.png"
