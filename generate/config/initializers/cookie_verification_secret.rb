60b4b1eea0bf7abae744b3e4bf89217c8ea695a5
Kyle Trantum <ktrantum@gmail.com>
2011-12-01 13:01:37 -0800

    removed all unnecessary nested applications

--- generate/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'ad211fffad6d595f76481492ca6d4c541e01f73ddf0d6533a664cfc4b3f8dcffe83a4c6600a37d4e2c31f9229a76c7687a646e763d8a533e7de3bcce1c01a74c';
--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '2ee3989837ca4412306f7731b448359c8d4406aba78ec34e52e609b93002414d1ddee21aa87fe6cd37ef7f7acff8aebb483512cfa6a056c7bd09097e9a9689e8';
