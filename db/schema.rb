4a620b59d1216b9d6fb5873ce561be4896418fd3
Cory Rothert <coryr32@gmail.com>
2011-11-30 23:29:27 -0800

    DeletedS

--- db/schema.rb
+++ db/schema.rb
@@ -12,26 +12,6 @@
 
 ActiveRecord::Schema.define(:version => 20111118155113) do
 
-  create_table "bet_links", :force => true do |t|
-    t.string   "bet_type"
-    t.integer  "bet_id"
-    t.integer  "wager"
-    t.boolean  "is_win"
-    t.string   "wager_selection"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "football_game_stats", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "football_team_stats", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "game_bets_h2hs", :force => true do |t|
     t.integer  "user_id"
     t.integer  "game_id"
@@ -44,33 +24,15 @@ ActiveRecord::Schema.define(:version => 20111118155113) do
   end
 
   create_table "games", :force => true do |t|
+    t.integer  "league_id"
     t.integer  "home_team_id"
     t.integer  "away_team_id"
-    t.string   "game_type"
-    t.integer  "league_id"
-    t.integer  "game_stat_id"
+    t.integer  "home_score"
+    t.integer  "away_score"
+    t.float    "spread"
+    t.float    "spread_check"
+    t.integer  "week"
     t.datetime "start_time"
-    t.datetime "end_time"
-    t.string   "location"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "winner_id"
-    t.integer  "loser_id"
-    t.integer  "winner_points"
-    t.integer  "loser_points"
-  end
-
-  create_table "general_bets", :force => true do |t|
-    t.string   "type"
-    t.integer  "item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "head2head_bets", :force => true do |t|
-    t.string   "type"
-    t.integer  "game_id"
-    t.boolean  "is_spread"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -86,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20111118155113) do
     t.string   "name"
     t.string   "short_name"
     t.string   "sport"
+    t.string   "logo_path"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -103,20 +66,6 @@ ActiveRecord::Schema.define(:version => 20111118155113) do
     t.boolean  "won_or_lost"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "overunder_bets", :force => true do |t|
-    t.string   "type"
-    t.integer  "game_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "photos", :force => true do |t|
-    t.string "description"
-    t.string "content_type"
-    t.string "filename"
-    t.binary "binary_data"
   end
 
   create_table "quarterbacks", :force => true do |t|
@@ -145,11 +94,25 @@ ActiveRecord::Schema.define(:version => 20111118155113) do
   end
 
   create_table "teams", :force => true do |t|
-    t.string   "name"
-    t.string   "conference"
     t.integer  "league_id"
-    t.string   "stat_type"
-    t.integer  "stat_id"
+    t.string   "conference"
+    t.string   "division"
+    t.string   "name"
+    t.string   "short_name"
+    t.string   "logo_path"
+    t.integer  "wins"
+    t.integer  "losses"
+    t.integer  "ties"
+    t.integer  "home_wins"
+    t.integer  "home_losses"
+    t.integer  "road_wins"
+    t.integer  "road_losses"
+    t.integer  "div_wins"
+    t.integer  "div_losses"
+    t.integer  "conf_wins"
+    t.integer  "conf_losses"
+    t.integer  "points_for"
+    t.integer  "points_against"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -162,6 +125,7 @@ ActiveRecord::Schema.define(:version => 20111118155113) do
     t.string   "email"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.integer  "money"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
