d86c2441c98c22c7ad5e66310e4a050ab7ad2e84
ckian <ckian@mail.csuchico.edu>
2011-12-04 14:51:09 -0800

    more fixes to games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,6 @@
 class GamesController < ApplicationController
   def index
     
-<<<<<<< HEAD
-
-=======
-    #@games = Games.find(:all)
->>>>>>> 3293ce42574a9f4a66ff9692eb138d4ff261841e
-    
   end
   
   def show
