c462f73f01b9f39906f7df4ecf3e36e7f9f5544e
ckian <ckian@mail.csuchico.edu>
2011-12-07 13:10:47 -0800

    added image uploads

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111118155113) do
+ActiveRecord::Schema.define(:version => 20111207045436) do
 
   create_table "game_bets_h2hs", :force => true do |t|
     t.integer  "user_id"
@@ -129,6 +130,10 @@ ActiveRecord::Schema.define(:version => 20111118155113) do
     t.integer  "money"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
