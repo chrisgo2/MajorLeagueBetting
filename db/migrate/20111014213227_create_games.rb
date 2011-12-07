1dc69db479c3b0861b3a54b2805e6584f2e31f92
Stephen Thomas <sthomas1618@gmail.com>
2011-12-07 07:21:04 -0800

    Fix for "completed" attribute of games table

--- db/migrate/20111014213227_create_games.rb
+++ db/migrate/20111014213227_create_games.rb
@@ -8,7 +8,7 @@ class CreateGames < ActiveRecord::Migration
       t.integer  :away_score
       t.float    :spread
       t.float    :spread_check
-      t.boolean  :complete
+      t.boolean  :completed
       t.integer  :week
       t.datetime :start_time
       
